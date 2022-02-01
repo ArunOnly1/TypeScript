509cd9ca0da74488e968521fbe231b7eabc9e063
ArunOnly1 <erarun1nly1@gmail.com>
2022-02-01 11:13:55 +0545

    tsconfig setting to es5

---
+++ info.js
@@ -0,0 +1,10 @@
+// private properties class bhitra matra accessible hunxa
+// public property class bahira bata access garna milxa
+
+// readonly le change garna didaina ekchoti initialize bhayepaxi
+
+// private propery ki constructor bata initialize garna paryo
+// ki ta method bata
+// aru le kei xuna ni sakdaina
+
+// protected le chahi child ma accessible hunxa
