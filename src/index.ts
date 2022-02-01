e1cb2b2180ce1796f0f1365ab85e0f86fc111113
ArunOnly1 <erarun1nly1@gmail.com>
2022-02-01 11:13:17 +0545

    inheritannce,access modifier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 class Department {
-  name: string
+  protected employees: string[] = []
 
-  constructor(n: string) {
-    this.name = n
-  }
+  constructor(private readonly id: string, private name: string) {}
 
   describe(this: Department) {
     console.log('Department :', this.name)
   }
+
+  addEmployee(...rest: string[]) {
+    this.employees.push(...rest)
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
 }
 
-const hm = new Department('Hotel Management')
-hm.describe()
+class ITDepartment extends Department {
+  constructor(id: string, private admin: string[]) {
+    super(id, 'IT')
+    this.employees = ['Ranjana', 'Deepesh']
+  }
+}
+
+const it = new ITDepartment('d2', ['Arun', 'Rajeev'])
+console.log(it)
